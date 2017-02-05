851830ff73556adc741ecc9787a7d24838dded1c
silzi <zilttu@gmail.com>
2017-02-05 19:39:45 +0200

    Added SQL Server connection and generated database

--- RetroRent/Models/IdentityModels.cs
+++ RetroRent/Models/IdentityModels.cs
@@ -20,6 +20,7 @@ namespace RetroRent.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet <Customer> Customers { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
